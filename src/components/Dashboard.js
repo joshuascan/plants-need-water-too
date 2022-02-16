3bc9937894d042761308200ae7cf93f5488ae45f
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-02-16 13:13:08 -0800

    adds fetchPlants to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchPlants } from "../store/plantSlice";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const plants = useSelector((state) => state.plants.plantsList);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPlants());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {plants.map((plant) => (
+        <div key={plant.plant_id}>{plant.nickname}</div>
+      ))}
+    </div>
+  );
 }
