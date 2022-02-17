b78d0427eb5598ad05fb6fae48bdd46632404be2
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-02-16 17:27:36 -0800

    adds mui app bar component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,11 @@
 import React from "react";
+import PrimarySearchAppBar from "./PrimarySearchAppBar";
 import PlantsDisplay from "./PlantsDisplay";
 
 export default function Dashboard() {
   return (
     <div>
+      <PrimarySearchAppBar />
       <PlantsDisplay />
     </div>
   );
