e903cda809002603f1f46eaf7c726544975a47c8
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-02-16 16:06:37 -0800

    creates basic layout for PlantCard

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,5 +1,40 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActionArea,
+} from "@mui/material";
 
 export default function PlantCard({ plant }) {
-  return <div>{plant.nickname}</div>;
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="250"
+          image={plant.img_url}
+          alt={plant.nickname}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {plant.nickname}
+          </Typography>
+          <Typography
+            sx={{ mb: 1.5, fontStyle: "italic" }}
+            color="text.secondary"
+          >
+            {plant.species}
+          </Typography>
+          <Typography>
+            Water{" "}
+            {plant.days_between_watering > 1
+              ? `every ${plant.days_between_watering} days`
+              : "everyday"}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
