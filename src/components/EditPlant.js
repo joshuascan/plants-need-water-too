423f8cebde520133c889ed64146aa09c60a74f39
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-05-27 17:35:26 -0700

    fixes plant routes

---
+++ src/components/EditPlant.js
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { editPlant } from "../store/plantSlice";
+import {
+  Box,
+  Button,
+  Grid,
+  Container,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+const initialFormValues = {
+  nickname: "",
+  species: "",
+  days_between_watering: "",
+  notes: "",
+};
+
+export default function EditPlant({ setIsAddPlantOpen }) {
+  const [editedPlant, setEditedPlant] = useState(initialFormValues);
+  const dispatch = useDispatch();
+
+  const handleChange = (event) => {
+    let { name, value } = event.target;
+    if (name === "days_between_watering") {
+      setEditedPlant({ ...editedPlant, [name]: Number(value) });
+    } else setEditedPlant({ ...editedPlant, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(editPlant(editedPlant));
+    setEditedPlant(initialFormValues);
+    setIsAddPlantOpen(false);
+  };
+
+  const handleCancel = () => {
+    setEditedPlant(initialFormValues);
+    setIsAddPlantOpen(false);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          mt: 16,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Add Plant
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 6 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="nickname"
+                label="Nickname"
+                name="nickname"
+                value={editedPlant.nickname}
+                onChange={handleChange}
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="species"
+                label="Species"
+                name="species"
+                value={editedPlant.species}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="daysBetweenWatering"
+                label="Days Between Watering"
+                name="days_between_watering"
+                value={editedPlant.days_between_watering}
+                onChange={handleChange}
+                type="number"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                multiline
+                id="notes"
+                label="Notes"
+                name="notes"
+                value={editedPlant.notes}
+                onChange={handleChange}
+                rows={4}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 5, mb: 2 }}
+          >
+            Add Plant
+          </Button>
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Button onClick={handleCancel}>Cancel</Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
