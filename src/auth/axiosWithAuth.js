16108b028424c0513e3d784e5d0c9a83d2a25e6e
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-06-01 12:51:13 -0700

    updates base URL to heroku link

--- src/auth/axiosWithAuth.js
+++ src/auth/axiosWithAuth.js
@@ -5,6 +5,6 @@ export const axiosWithAuth = () => {
     headers: {
       Authorization: localStorage.getItem("token"),
     },
-    baseURL: "http://localhost:4000",
+    baseURL: "https://ft-watermyplants-1.herokuapp.com",
   });
 };
