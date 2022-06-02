a54b4ec5a00d1c4da4dd01fcec4ec0eb1bb98082
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-06-02 16:09:48 -0700

    adjusts margin on sign in page

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -77,7 +77,7 @@ export default function SignIn() {
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
-              my: 8,
+              my: 12,
               mx: 4,
               display: "flex",
               flexDirection: "column",
